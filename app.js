7823ce49cd2747326e6cf3c2fc9026ea41909eda
hudsonviana <hudson1206@gmail.com>
2024-01-14 19:02:06 -0300

    Adding Middleware & Layouts

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Parsing middleware
-// Parse application
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Static files
+app.use(express.static('public'));
+
+// Template Engine
+app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+// Router
+app.get('/', (req, res) => {
+  res.render('home');
+});
 
 app.listen(port, () => console.log('App running at port 5000'));
