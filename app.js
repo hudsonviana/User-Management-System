3ec22daad5bdfdb7d23379aad20e544e5f763ee0
hudsonviana <hudson1206@gmail.com>
2024-01-14 20:23:56 -0300

    Database Setup

--- app.js
+++ app.js
@@ -21,6 +21,23 @@ app.use(express.static('public'));
 app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 
+// Database Connection
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Conecta ao banco de dados
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão bem-sucedida ao banco de dados!');
+});
+
 // Router
 app.get('/', (req, res) => {
   res.render('home');
